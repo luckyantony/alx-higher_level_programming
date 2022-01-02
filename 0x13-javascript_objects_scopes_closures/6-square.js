cc6fa556015dbd33e671d84c78c7f7b1e2c11363
luckyantony <83889059+luckyantony@users.noreply.github.com>
2022-01-02 22:24:17 +0300

    Corrections

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+// class Square
+const Rectangle = require('./5-square');
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write(c);
+      }
+      if (i < this.height) {
+        process.stdout.write('\n');
+      }
+    }
+  }
+};
---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// returns the reversed version of a list
+exports.esrever = function (list) {
+    const rev = [];
+    for (let i = 0; i < list.length; i++) {
+      rev.unshift(list[i]);
+    }
+    return rev;
+  };
