578896867f2e64a6122bc67e255986bf4b710b67
luckyantony <83889059+luckyantony@users.noreply.github.com>
2022-01-24 20:20:48 +0300

    Python networks

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" what's my status """
+import requests
+
+
+if __name__ == "__main__":
+    r = requests.get('https://intranet.hbtn.io/status')
+    print('Body response:')
+    print('\t- type: {}'.format(type(r.text)))
